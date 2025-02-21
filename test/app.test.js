0d23a7cfb01f83f02042b0b704371431cd9f1ee4
beniye19 <beniyemwangi@gmail.com>
2025-02-22 01:35:34 +0300

    finished test

--- test/app.test.js
+++ test/app.test.js
@@ -3,3 +3,15 @@ const app = require("../app");
 
 describe("GET /", function () {
   it("should return HTML content with 'Hello, World!'", function (done) {
+    request(app)
+      .get("/")
+      .expect("Content-Type", /html/)
+      .expect(200)
+      .expect((res) => {
+        if (!res.text.includes("Hello, World!")) {
+          throw new Error("Response does not contain 'Hello, World!'");
+        }
+      })
+      .end(done);
+  });
+});
