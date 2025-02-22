415808da392671da599f1bb8c6256f143dbc5ca4
beniye19 <beniyemwangi@gmail.com>
2025-02-22 08:47:55 +0300

    Add a user data script for Grafana.

--- Prometheus-Grafana/compute.tf
+++ Prometheus-Grafana/compute.tf
@@ -21,3 +21,17 @@ resource "aws_instance" "Grafana" {
 
   tags = {
     Name = "Grafana"
+  }
+}
+
+# AWS instance for Prometheus
+resource "aws_instance" "Prometheus" {
+  ami                    = data.aws_ami.server_ami.id
+  instance_type          = var.prometheus_instance
+  vpc_security_group_ids = [aws_security_group.prometheus_sg.id]
+
+
+  tags = {
+    Name = "Prometheus"
+  }
+}
