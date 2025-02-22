a1503985166fd6bc0b56f44ad641d3f3fb21caf8
beniye19 <beniyemwangi@gmail.com>
2025-02-22 08:55:04 +0300

    node exporter 9100 ingress added

--- Prometheus-Grafana/security.tf
+++ Prometheus-Grafana/security.tf
@@ -4,4 +4,9 @@
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
+}  ingress {
+    description = "Node Exporter"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
